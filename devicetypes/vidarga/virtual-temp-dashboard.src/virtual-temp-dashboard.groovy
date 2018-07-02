0a88d8d035ca10cf437cfdc24dd0d2e43da16e58
Vidar Gaasø <vidar.gaasoe@gmail.com>
2018-07-02 08:30:39 +0200

    --

--- devicetypes/vidarga/virtual-temp-dashboard.src/virtual-temp-dashboard.groovy
+++ devicetypes/vidarga/virtual-temp-dashboard.src/virtual-temp-dashboard.groovy
@@ -21,10 +21,10 @@ metadata {
     	attribute "Temp2","number"
     	attribute "Temp3","number"
 
-	command "changeTemp1", ["number"]
+		command "changeTemp1", ["number"]
     	command "changeTemp2", ["number"]
     	command "changeTemp3", ["number"]
-      command "setTemperature", ["number"]
+      	command "setTemperature", ["number"]
 	}
 
 	preferences {
