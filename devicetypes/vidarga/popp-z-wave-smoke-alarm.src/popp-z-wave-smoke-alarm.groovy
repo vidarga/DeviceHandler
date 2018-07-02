8439a041f10411a417b9e72e5743d348fad44ecd
vidarga <38034207+vidarga@users.noreply.github.com>
2018-07-02 14:30:01 +0200

    Update popp-z-wave-smoke-alarm.groovy

--- devicetypes/vidarga/popp-z-wave-smoke-alarm.src/popp-z-wave-smoke-alarm.groovy
+++ devicetypes/vidarga/popp-z-wave-smoke-alarm.src/popp-z-wave-smoke-alarm.groovy
@@ -9,10 +9,10 @@
  *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
  *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
  *  for the specific language governing permissions and limitations under the License.
- *
+ *, runLocally: false, minHubCoreVersion: '000.017.0012', executeCommandsLocally: false
  */
 metadata {
-	definition (name: "popp-z-wave-smoke-alarm", namespace: "vidarga", author: "vidarga", runLocally: false, minHubCoreVersion: '000.017.0012', executeCommandsLocally: false) {
+	definition (name: "popp-z-wave-smoke-alarm", namespace: "vidarga", author: "vidarga") {
 		capability "Smoke Detector"
 		capability "Sensor"
 		capability "Battery"
